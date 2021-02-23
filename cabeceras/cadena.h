572365a90e4b3f885e5395052197a619d85314e5
Damian Ariel Marotte <drd00m@msn.com>
2021-02-23 00:35:49 -0300

    Agrego declaraciones de proposito.

--- cabeceras/cadena.h
+++ cabeceras/cadena.h
@@ -9,14 +9,19 @@
 #define MINSUGERENCIAS 5
 #define MAXLONGITUD 20
 
+// Arreglo de caracteres anchos.
 typedef wchar_t* String;
 
+// Convierte una cadena a minusculas.
 void toLower(String s);
 
+// Determina si un caracter es un delimitador.
 int isDelimiter(wchar_t c);
 
+// Determina si una cadena esta en un arreglo.
 int enArreglo(String s, String* a);
 
+// Copia una cadena a un arreglo.
 void arregloAgregar(String s, String* a);
 
 #endif
--- cabeceras/sugerencias.h
+++ cabeceras/sugerencias.h
@@ -14,6 +14,9 @@ int reemplazarCaracteres(String s, TablaHash* t, String* a, String* b);
 
 int agregarEspacios(String s, TablaHash* t, String* b);
 
+// Si la palabra se encuentra en el diccionario y no se encuentra en el
+// arreglo de palabras ya sugeridas, entonces la muestra por pantalla y
+// la agrega a dicho arreglo. Deuvuelve 1 en tal caso y 0 en caso contrario.
 int sugerirPalabra(String s, TablaHash* t, String* b);
 
 void chequearPalabra(String s, unsigned ln, TablaHash* t);
